d68ae5c0bdd81afed969ff3b604da3155a30b433
Priyank Vadadoriya <priyankvaddoriya10@gmail.com>
2026-07-29 09:32:08 +0530

    Add error catcher to debug 500 error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,16 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+class ApplicationController < ActionController::Base
+  # Prevent 500 error blank screen and show exact error for debugging
+  rescue_from StandardError do |exception|
+    if Rails.env.production?
+      render plain: "EXACT ERROR: #{exception.message} \n\n BACKTRACE: \n#{exception.backtrace.join("\n")}", status: 500
+    else
+      raise exception
+    end
+  end
+end
+
 end
