a39a6e6ebe6942b7f10db82c961deecd56d17089
Priyank Vadadoriya <priyankvaddoriya10@gmail.com>
2026-07-29 09:55:00 +0530

    Update routes for custom passwords controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { passwords: 'users/passwords' }
   resources :expenses do
     collection do
       post :set_budget
